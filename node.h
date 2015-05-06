80fdf9b7bae2337b29ea620a0ff1282701656d89
Dawid Dylowicz <ddylowicz@gmail.com>
2015-05-06 21:12:27 +0200

    Added include in Transition

--- node.h
+++ node.h
@@ -1,8 +1,6 @@
 #ifndef NODE_H
 #define NODE_H
 
-#include "node.h"
-
 class Node
 {
 public:
--- transition.h
+++ transition.h
@@ -1,6 +1,7 @@
 #ifndef TRANSITION_H
 #define TRANSITION_H
 
+#include "node.h"
 
 class Transition : public Node
 {

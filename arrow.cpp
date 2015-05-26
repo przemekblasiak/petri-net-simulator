b66570a65a41e73eb3dcef6c5206dbf8eb2e4906
Mateusz Drzazga <m.drzazga3@gmail.com>
2015-05-26 22:43:32 +0200

    Improved arrow drawing(still no arrow head)

--- arrow.cpp
+++ arrow.cpp
@@ -27,48 +27,84 @@ void Arrow::clear()
     fromPlaceToTransition = true;
 }
 
-void Arrow::draw(QPainter &painter) const {
-    QPoint point1, point2;
-    int w1, h1, w2, h2, dh, dw;
+void Arrow::draw(QPainter &painter) const {    
+    QPoint placeMiddle = QPoint(this->place->x() + this->place->width()/2, this->place->y() + this->place->height()/2);
+    QPoint transitionMiddle = QPoint(this->transition->x() + this->transition->width()/2, this->transition->y() + this->transition->height()/2);
+    QPoint *middle1, *middle2;
+    int height1, height2, width1, width2;
     if (this->fromPlaceToTransition) {
-        point1 = QPoint(this->place->x(), this->place->y());
-        w1 = this->place->width();
-        h1 = this->place->height();
-        point2 = QPoint(this->transition->x(), this->transition->y());
-        w2 = this->transition->width();
-        h2 = this->transition->height();
+        middle1 = &placeMiddle;
+        height1 = this->place->height();
+        width1 = this->place->width();
+        middle2 = &transitionMiddle;
+        height2 = this->transition->height();
+        width2 = this->transition->width();
     }
     else {
-        point1 = QPoint(this->transition->x(), this->transition->y());
-        w1 = this->transition->width();
-        h1 = this->transition->height();
-        point2 = QPoint(this->place->x(), this->place->y());
-        w2 = this->place->width();
-        h2 = this->place->height();
-    }
-    dh = (point1.y() - point2.y()) - h2/2 + h1/2;
-    dw = point2.x() - point1.x() - w1;
-
-    // Prawo-Lewo
-    // TODO naprawde pomoglyby QPointy w transition i place
-    QVector<QPoint> checkPoints;
-    bool toRight = point2.x() > point1.x();
-    if (toRight) {
-        checkPoints.append(QPoint(point1.x() + w1, point1.y() + h1/2));
-        int d1 = dw/2;
-        checkPoints.append(QPoint(checkPoints.last().x() + d1, checkPoints.last().y()));
-
-        checkPoints.append(checkPoints.last());
-        checkPoints.append(QPoint(checkPoints.last().x(), checkPoints.last().y() - dh)); // TODO moze byc problem z wysokoscia
-
-        checkPoints.append(checkPoints.last());
-        int d2 = dw - d1;
-        checkPoints.append(QPoint(checkPoints.last().x() + d2, checkPoints.last().y()));
+        middle1 = &transitionMiddle;
+        height1 = this->transition->height();
+        width1 = this->transition->width();
+        middle2 = &placeMiddle;
+        height2 = this->place->height();
+        width2 = this->place->width();
     }
 
-    // TODO Góra-Dół
+    int dy = middle2->y() - middle1->y();
+    int dx = middle2->x() - middle1->x();
+    int y1Offset = dy > 0 ? height1/2 : -height1/2;
+    int y2Offset = dy > 0 ? height2/2 : -height2/2;
+    int x1Offset = dx > 0 ? width1/2 : -width1/2;
+    int x2Offset = dx > 0 ? width2/2 : -width2/2;
+
+    bool vertical = true;
+    if (abs(dx) > abs(dy)) {
+        vertical = false;
+    }
+
+    QVector<QPoint> checkPoints(6);
+    if (vertical) {
+        bool overlapsX = middle1->x() >= (middle2->x() - width2/2) && middle1->x() <= (middle2->x() + width2/2);
+        if (overlapsX) {
+            int yDistance = dy - y1Offset - y2Offset;
+            checkPoints.append(QPoint(middle1->x(), middle1->y() + y1Offset));
+            checkPoints.append(QPoint(checkPoints.last().x(), checkPoints.last().y() + yDistance/2));
+
+            checkPoints.append(checkPoints.last());
+            checkPoints.append(QPoint(checkPoints.last().x() + dx, checkPoints.last().y()));
+
+            checkPoints.append(checkPoints.last());
+            checkPoints.append(QPoint(checkPoints.last().x(), checkPoints.last().y() + (yDistance - yDistance/2)));
+        }
+        else {
+            checkPoints.append(QPoint(middle1->x(), middle1->y() + y1Offset));
+            checkPoints.append(QPoint(checkPoints.last().x(), checkPoints.last().y() + (dy - y1Offset)));
+
+            checkPoints.append(checkPoints.last());
+            checkPoints.append(QPoint(checkPoints.last().x() + (dx - x2Offset), checkPoints.last().y()));
+        }
+    }
+    else {
+        bool overlapsY = middle1->y() >= (middle2->y() - height2/2) && middle1->y() <= (middle2->y() + height2/2);
+        if (overlapsY) {
+            int xDistance = dx - x1Offset - x2Offset;
+            checkPoints.append(QPoint(middle1->x() + x1Offset, middle1->y()));
+            checkPoints.append(QPoint(checkPoints.last().x() + xDistance/2, checkPoints.last().y()));
+
+            checkPoints.append(checkPoints.last());
+            checkPoints.append(QPoint(checkPoints.last().x(), checkPoints.last().y() + dy));
+
+            checkPoints.append(checkPoints.last());
+            checkPoints.append(QPoint(checkPoints.last().x() + (xDistance - xDistance/2), checkPoints.last().y()));
+        }
+        else {
+            checkPoints.append(QPoint(middle1->x() + x1Offset, middle1->y()));
+            checkPoints.append(QPoint(checkPoints.last().x() + (dx - x1Offset), checkPoints.last().y()));
+
+            checkPoints.append(checkPoints.last());
+            checkPoints.append(QPoint(checkPoints.last().x(), checkPoints.last().y() + (dy - y2Offset)));
+        }
+    }
 
     painter.drawLines(checkPoints);
-    qDebug() << checkPoints;
 }
 
